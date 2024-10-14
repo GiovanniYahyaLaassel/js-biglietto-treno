fc1e1fec03917e046daac7de52d8f98e544dc4fc
Laassel <lassel.giovanni@gmail.com>
2024-10-14 15:34:54 +0200

    creazioni delle variabili

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const bomberina = "HELLO WORLD";
-console.log(bomberina);
+//CREAZIONE DELLE VARIABILI 
+
+//Chiedere all'utente il numero di chilometri da percorrere
+const chilometri = prompt("Quanti chilometri vorresti percorrere");
+
+//Chiedere all'utente l'età del passegero
+const età = prompt("Quanti anni hai?");
+
+//Impostare il prezzo al chilometro e calcolare il totale prima degli sconti
+const prezzoAlChilometro = 0.21;
